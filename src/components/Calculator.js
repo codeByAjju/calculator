6bd7f02b576b20c9b70c7cfa6499023dda3fbcec
ajju patel <coderajju@gmail.com>
2023-06-06 06:30:20 -0700

    back button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,19 +17,23 @@ const Calculator = () => {
       setResult(calculatedResult.toString());
   };
 
+  const handleBack = () => {
+    setResult(result.slice(0, -1));
+  };
+    
   return (
-    <div className="calculator col-lg-4 mt-3" style={{borderRadius:70,backgroundColor:"black"}}>
+    <div className="calculator col-lg-4 mt-3" style={{borderRadius:70,backgroundColor:"dark"}}>
       <input type="text" className="result mt-2" value={result} readOnly style={{color:"white",width:450,height:55,fontSize:35,outline:"none",border:"none",backgroundColor:"black"}} />
       <div className="buttons mt-2 text-center" style={{alignItems:"center"}}>
         <button className='bg-dark border text-white' onClick={() => handleButtonPress('7')}>7</button>
         <button className='bg-dark border text-white' onClick={() => handleButtonPress('8')}>8</button>
         <button className='bg-dark border text-white' onClick={() => handleButtonPress('9')}>9</button>
         <button className='bg-warning' onClick={() => handleButtonPress('+')}>+</button>
-        <br/>
+        <br/> 
         <button onClick={() => handleButtonPress('4')}>4</button>
         <button onClick={() => handleButtonPress('5')}>5</button>
         <button onClick={() => handleButtonPress('6')}>6</button>
-        <button className='bg-warning' onClick={() => handleButtonPress('-')}>-</button>
+        <button className='bg-warning' on Click={() => handleButtonPress('-')}>-</button>
         <br/>
         <button onClick={() => handleButtonPress('1')}>1</button>
         <button onClick={() => handleButtonPress('2')}>2</button>
@@ -41,6 +45,8 @@ const Calculator = () => {
         <button onClick={handleClear}>C</button>
         <button onClick={handleCalculate}>=</button>
         <button className='bg-warning' onClick={() => handleButtonPress('/')}>/</button>
+        <br/>
+        <button className='bg-warning' onClick={handleBack}>X</button>
       </div>
     </div>
   );
